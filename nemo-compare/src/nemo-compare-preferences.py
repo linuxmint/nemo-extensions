81655a49fd0ed78ac6f6e57bbb01729c64ffd754
Eduard Dopler <kontakt@eduard-dopler.de>
2015-11-02 14:06:14 +0100

    Use GTK+ through PyGObject instead of deprecated PyGTK
    This change makes the preferences compatible with both Python 2 and 3.

--- nemo-compare/src/nemo-compare-preferences.py
+++ nemo-compare/src/nemo-compare-preferences.py
@@ -17,8 +17,9 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import gettext
-import gtk
 import locale
+
+from gi.repository import Gtk
 
 import utils
 
@@ -33,7 +34,7 @@ def combo_add_and_select(combo, text):
 
 
 class NemoCompareExtensionPreferences:
-    """The main class for a preferences dialog using PyGTK."""
+    """The main class for a preferences dialog using GTK+ through PyGObject."""
 
     combo = None
     combo_3way = None
@@ -41,14 +42,14 @@ class NemoCompareExtensionPreferences:
 
     def cancel_event(self, widget, event, data=None):
         """Quit the program."""
-        gtk.main_quit()
+        Gtk.main_quit()
         return False
 
     def changed_cb(self, combo):
         """Any of the comboboxes has changed, change the data accordingly."""
         model = combo.get_model()
         index = combo.get_active()
-        entry = combo.get_child().get_text()
+        entry = combo.get_active_text()
 
         selection = ""
 
@@ -69,7 +70,7 @@ class NemoCompareExtensionPreferences:
     def save_event(self, widget, event, data=None):
         """Save settings and quit program."""
         self.config.save()
-        gtk.main_quit()
+        Gtk.main_quit()
         return False
 
     def __init__(self):
@@ -87,22 +88,21 @@ class NemoCompareExtensionPreferences:
         gettext.textdomain(utils.APP)
         _ = gettext.gettext
 
-        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        #self.window.set_position(gtk.WIN_POS_CENTER)
-        icon = self.window.render_icon(gtk.STOCK_PREFERENCES,
-                                       gtk.ICON_SIZE_DIALOG)
+        self.window = Gtk.Window()
+        icon = self.window.render_icon(Gtk.STOCK_PREFERENCES,
+                                       Gtk.IconSize.DIALOG)
         self.window.set_icon(icon)
         self.window.set_resizable(False)
         self.window.set_title(_("Nemo Compare Extension Preferences"))
         self.window.connect("delete_event", self.cancel_event)
         self.window.set_border_width(15)
 
-        main_vbox = gtk.VBox(False, 0)
+        main_vbox = Gtk.VBox(False, 0)
         self.window.add(main_vbox)
 
         # normal diff
-        frame = gtk.Frame(_("Normal Diff"))
-        self.combo = gtk.combo_box_entry_new_text()
+        frame = Gtk.Frame(label=_("Normal Diff"))
+        self.combo = Gtk.ComboBoxText()
         for text in self.config.engines:
             if text == self.config.diff_engine:
                 combo_add_and_select(self.combo, text)
@@ -113,8 +113,8 @@ class NemoCompareExtensionPreferences:
         main_vbox.pack_start(frame, True, True, 0)
 
         # 3-way diff
-        frame_3way = gtk.Frame(_("Three-Way Diff"))
-        self.combo_3way = gtk.combo_box_entry_new_text()
+        frame_3way = Gtk.Frame(label=_("Three-Way Diff"))
+        self.combo_3way = Gtk.ComboBoxText()
         for text in self.config.engines:
             if text == self.config.diff_engine_3way:
                 combo_add_and_select(self.combo_3way, text)
@@ -125,8 +125,8 @@ class NemoCompareExtensionPreferences:
         main_vbox.pack_start(frame_3way, True, True, 0)
 
         # n-way diff
-        frame_multi = gtk.Frame(_("N-Way Diff"))
-        self.combo_multi = gtk.combo_box_entry_new_text()
+        frame_multi = Gtk.Frame(label=_("N-Way Diff"))
+        self.combo_multi = Gtk.ComboBoxText()
         for text in self.config.engines:
             if text == self.config.diff_engine_multi:
                 combo_add_and_select(self.combo_multi, text)
@@ -136,20 +136,20 @@ class NemoCompareExtensionPreferences:
         frame_multi.add(self.combo_multi)
         main_vbox.pack_start(frame_multi, True, True, 0)
 
-        separator = gtk.HBox(False, 5)
+        separator = Gtk.HBox(False, 5)
         main_vbox.pack_start(separator, False, True, 5)
 
         # cancel / ok
-        confirm_hbox = gtk.HBox(False, 0)
+        confirm_hbox = Gtk.HBox(False, 0)
         main_vbox.pack_start(confirm_hbox, False, False, 0)
 
-        cancel_button = gtk.Button(stock=gtk.STOCK_CANCEL)
+        cancel_button = Gtk.Button(stock=Gtk.STOCK_CANCEL)
         cancel_button.connect_object("clicked",
                                      self.cancel_event,
                                      self.window, None)
         confirm_hbox.pack_start(cancel_button, True, True, 5)
 
-        ok_button = gtk.Button(stock=gtk.STOCK_OK)
+        ok_button = Gtk.Button(stock=Gtk.STOCK_OK)
         ok_button.connect_object("clicked",
                                  self.save_event,
                                  self.window, None)
@@ -159,7 +159,7 @@ class NemoCompareExtensionPreferences:
 
     def main(self):
         """GTK main method."""
-        gtk.main()
+        Gtk.main()
 
 
 if __name__ == "__main__":
