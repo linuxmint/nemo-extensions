602d53302406803b89c5252eecba593b77cb2363
Eduard Dopler <kontakt@eduard-dopler.de>
2015-12-02 00:57:42 +0100

    compare: Set new version in setup.py
    As nemo-compare underwent many changes and is considered refactored for
    future releases, set the package version to 3.0.0.
    Also this helps distinguish this version from prior ones built for the
    python2 base.
    
    Note that the changelog file in debian/ has to be edited before packages
    can be built with this new version number.

--- nemo-compare/setup.py
+++ nemo-compare/setup.py
@@ -21,7 +21,7 @@ data = [
 
 DistUtilsExtra.auto.setup(
     name         = "nemo-compare",
-    version      = "2.8.0",
+    version      = "3.0.0",
     description  = "Nemo extension for file comparison via context menu.",
     author       = "Eduard Dopler",
     author_email = "kontakt@eduard-dopler.de",
