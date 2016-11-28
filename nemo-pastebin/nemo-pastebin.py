024d11f25287248195fe41dd98b4d485ac6a1519
leigh123linux <leigh123linux@googlemail.com>
2016-11-28 23:48:19 +0000

    nemo-pastebin: remove _ from get_name_and_desc

--- nemo-pastebin/nemo-pastebin.py
+++ nemo-pastebin/nemo-pastebin.py
@@ -193,4 +193,4 @@ class PastebinitExtension(GObject.GObject, Nemo.MenuProvider, Nemo.NameAndDescPr
                 Gtk.main_iteration()
 
     def get_name_and_desc(self):
-        return [_("Nemo Pastebin:::Send files to a paste service via the context menu")]
+        return [("Nemo Pastebin:::Send files to a paste service via the context menu")]
