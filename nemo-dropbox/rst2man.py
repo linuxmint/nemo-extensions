a9e3c0215bad42b61e2ac226e7090a7b5a2ff593
Eli Schwartz <eschwartz93@gmail.com>
2018-04-30 14:47:52 -0500

    Misc cleanup (#275)
    * nemo-preview: rename evince.js to xreader.js
    
    Since this was ported from evince to xreader, it makes sense to rename
    the source file to match.
    
    * Remove junk files committed to git.
    
    * pep 394: specify python2 in shebangs which already need python2
    
    This continues to work as expected on distros where python is python2,
    and avoids the need to patch the files on distros where python is
    python3.

--- nemo-dropbox/rst2man.py
+++ nemo-dropbox/rst2man.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python2
 
 # Author:
 # Contact: grubert@users.sf.net
--- nemo-python/docs/xsl/fixxref.py
+++ nemo-python/docs/xsl/fixxref.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python2
 # -*- Mode: Python; py-indent-offset: 4 -*-
 
 import getopt
