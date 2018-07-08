8c800cea3da8089e98650a00dc110d194d2ddb51
Michael Webster <miketwebster@gmail.com>
2018-07-08 19:13:18 -0400

    nemo-audio-tab: port to python3, clean up build

--- nemo-audio-tab/nemo-extension/nemo-audio-tab.py
+++ nemo-audio-tab/nemo-extension/nemo-audio-tab.py
@@ -1,16 +1,16 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 
 # change log:
 # RavetcoFX: Forked from nemo-media-columns and nemo-emblems
 
-import urllib
+from urllib import parse
 import locale, gettext, os
 import mutagen
 
 from gi.repository import GObject, Gio, Gtk, Nemo
 # for id3 support
 from mutagen.easyid3 import EasyID3
-from mutagen.mp3 import MPEGInfo
+from mutagen.mp3 import MP3
 from mutagen.flac import FLAC, StreamInfo
 
 class AudioPropertyPage(GObject.GObject, Nemo.PropertyPageProvider, Nemo.NameAndDescProvider):
@@ -18,19 +18,19 @@ class AudioPropertyPage(GObject.GObject, Nemo.PropertyPageProvider, Nemo.NameAnd
     def get_property_pages(self, files):
         # files: list of NemoVFSFile
         if len(files) != 1:
-            return
+            return []
 
         file = files[0]
         if file.get_uri_scheme() != 'file':
-            return
+            return []
 
         if file.is_directory():
-            return
+            return []
 
         if not(file.is_mime_type('audio/mpeg') or file.is_mime_type('audio/flac')):
-            return
+            return []
 
-        filename = urllib.unquote(file.get_uri()[7:])
+        filename = parse.unquote(file.get_uri()[7:])
 
         #GUI
         locale.setlocale(locale.LC_ALL, '')
@@ -43,7 +43,7 @@ class AudioPropertyPage(GObject.GObject, Nemo.PropertyPageProvider, Nemo.NameAnd
 
         self.builder = Gtk.Builder()
         self.builder.set_translation_domain('nemo-extensions')
-        self.builder.add_from_file("/usr/share/nemo-python/extensions/nemo-audio-tab.glade")
+        self.builder.add_from_file("/usr/share/nemo-audio-tab/nemo-audio-tab.glade")
 
         #connect gtk objects to python variables
         for obj in self.builder.get_objects():
@@ -107,14 +107,16 @@ class AudioPropertyPage(GObject.GObject, Nemo.PropertyPageProvider, Nemo.NameAnd
 
                 # try to read MP3 information (bitrate, length, samplerate)
             try:
-                mpinfo = MPEGInfo(open(filename))
-                file.add_string_attribute('bitrate', str(mpinfo.bitrate/1000) + " Kbps")
+                with open(filename, 'rb') as mpfile:
+                    mpinfo = MP3(mpfile).info
+                    file.add_string_attribute('bitrate', str(mpinfo.bitrate/1000) + " Kbps")
 
-                file.add_string_attribute('samplerate', str(mpinfo.sample_rate) + " Hz")
-                # [SabreWolfy] added consistent formatting of times in format hh:mm:ss
-                mp3length = "%02i:%02i:%02i" % ((int(mpinfo.length/3600)), (int(mpinfo.length/60%60)), (int(mpinfo.length%60)))
-                file.add_string_attribute('length', mp3length)
-            except:
+                    file.add_string_attribute('samplerate', str(mpinfo.sample_rate) + " Hz")
+                    # [SabreWolfy] added consistent formatting of times in format hh:mm:ss
+                    mp3length = "%02i:%02i:%02i" % ((int(mpinfo.length/3600)), (int(mpinfo.length/60%60)), (int(mpinfo.length%60)))
+                    file.add_string_attribute('length', mp3length)
+            except Exception as e:
+                print(e)
                 file.add_string_attribute('bitrate', no_info)
                 file.add_string_attribute('length', no_info)
                 file.add_string_attribute('samplerate', no_info)
@@ -183,7 +185,11 @@ class AudioPropertyPage(GObject.GObject, Nemo.PropertyPageProvider, Nemo.NameAnd
         self.builder.get_object("encoded_by_text").set_label(file.get_string_attribute('encodedby'))
         self.builder.get_object("copyright_text").set_label(file.get_string_attribute('copyright'))
 
-        return Nemo.PropertyPage(name="NemoPython::audio", label=self.property_label, page=self.mainWindow),
+        return [
+            Nemo.PropertyPage(name="NemoPython::audio",
+                              label=self.property_label,
+                              page=self.builder_root_widget)
+        ]
 
     def get_name_and_desc(self):
         return [("Nemo Audio Tab:::View audio tag information from the properties tab")]
--- nemo-audio-tab/setup.py
+++ nemo-audio-tab/setup.py
@@ -1,16 +1,11 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 
-
-import os
-import sys
-
-import DistUtilsExtra.auto
+from setuptools import setup
 
 # Create data files
-data = [ ('/usr/share/nemo-python/extensions', ['nemo-extension/nemo-audio-tab.py', 'nemo-extension/nemo-audio-tab.glade']) ]
 
 # Setup stage
-DistUtilsExtra.auto.setup(
+setup(
     name         = "nemo-audio-tab",
     version      = "3.8.0",
     description  = "View audio tag information from the file manager's properties tab",
@@ -18,5 +13,22 @@ DistUtilsExtra.auto.setup(
     author_email = "root@linuxmint.com",
     url          = "https://github.com/linuxmint/nemo-extensions",
     license      = "GPL3",
-    data_files   = data
+
+    # See debian/control for install-depends - this is useless here, except as reference.
+    # install_requires (and only works with python modules.  It's equally as bad to have
+    # some deps here, then draw them into debian/control using {python3:Depends} and add
+    # them to our non-python depends.
+
+    # install_requires = [
+    #     'gir1.2-nemo-3.0',
+    #     'gir1.2-gtk-3.0',
+    #     'gir1.2-glib-2.0',
+    #     'python-nemo',
+    #     'python3-mutagen'
+    # ]
+
+    data_files   = [
+        ('/usr/share/nemo-python/extensions', ['nemo-extension/nemo-audio-tab.py']),
+        ('/usr/share/nemo-audio-tab',         ['nemo-extension/nemo-audio-tab.glade'])
+    ]
 )
