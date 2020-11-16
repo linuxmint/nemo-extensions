72ab3c2e2e1199f52cba0993962fa9212ce3c053
szaszaiz <37189315+szaszaiz@users.noreply.github.com>
2020-11-16 20:31:55 +0100

    Add support for Zstandard archives
    Closes #376.

--- nemo-fileroller/src/nemo-fileroller.c
+++ nemo-fileroller/src/nemo-fileroller.c
@@ -197,7 +197,9 @@ static struct {
 		{ "application/x-zip", TRUE },
 		{ "application/x-zip-compressed", TRUE },
 		{ "application/x-zoo", TRUE },
+		{ "application/x-zstd-compressed-tar", TRUE },
 		{ "application/zip", TRUE },
+		{ "application/zstd", TRUE },
 		{ "multipart/x-zip", TRUE },
 		{ NULL, FALSE }
 };
