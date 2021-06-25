324c652ac1a8ad9f6c7602979e4494c3af239ea1
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2021-06-25 17:09:31 +0100

    nemo-compare 5.0.1

--- nemo-compare/setup.py
+++ nemo-compare/setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 # Setup stage
 setup(
     name         = "nemo-compare",
-    version      = "5.0.0",
+    version      = "5.0.1",
     description  = "Context menu comparison extension for Nemo file manager",
     author       = "Linux Mint",
     author_email = "root@linuxmint.com",
