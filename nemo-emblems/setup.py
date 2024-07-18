710884d40df5d93453e58849a216c2c145a0e537
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2024-07-18 20:04:08 +0100

    nemo-emblems 6.2.1

--- nemo-emblems/setup.py
+++ nemo-emblems/setup.py
@@ -23,7 +23,7 @@ from setuptools import setup
 # Setup stage
 setup( packages=[],
     name         = "nemo-emblems",
-    version      = "6.2.0",
+    version      = "6.2.1",
     description  = "Change your folder and file emblems",
     author       = "Linux Mint",
     author_email = "root@linuxmint.com",
