745c85137bc966121de7f87d6717f7bd688c4090
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2026-09-22 15:54:55 +0100

    6.7.2-unstable

--- nemo-emblems/setup.py
+++ nemo-emblems/setup.py
@@ -23,7 +23,7 @@ from setuptools import setup
 # Setup stage
 setup( packages=[],
     name         = "nemo-emblems",
-    version      = "6.7.1",
+    version      = "6.7.2",
     description  = "Change your folder and file emblems",
     author       = "Linux Mint",
     author_email = "root@linuxmint.com",
